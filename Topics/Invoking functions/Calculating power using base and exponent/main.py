2d95da737f7ec4d837b4245205a3e18142d67fdd
Salome <bibichadzesalome9@gmail.com>
2025-04-26 03:09:12 +0400

    Initial commit

---
+++ Topics/Invoking functions/Calculating power using base and exponent/main.py
@@ -0,0 +1,10 @@
+# Import the necessary function
+def result(a, b):
+    return a ** b
+
+base = int(input())
+exponent = int(input())
+
+# Use the pow() function to calculate the result
+
+print(result(base, exponent))
